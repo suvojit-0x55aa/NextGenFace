3955cdd1c765ecf9cc5fffc1ec3d8346be5617ec
Suvojit <davsuvo@gmail.com>
2026-02-24 14:37:41 +0530

    refactor: move optim to src/optim/
    Move config, pipeline, optimizer, textureloss to src/optim/.
    Update all cross-module imports to use new package paths.
    Replace mkdir_p with os.makedirs(exist_ok=True). All 150 tests pass.

--- src/optim/__init__.py
+++ src/optim/__init__.py
@@ -1,1 +1,5 @@
-# Populated after file moves
+from optim.config import Config
+from optim.pipeline import Pipeline
+from optim.optimizer import Optimizer
+
+__all__ = ["Config", "Pipeline", "Optimizer"]
